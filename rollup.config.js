bea79e7593b4d829d96311dd004fa6be84e13389
saojun1024 <1341313944@qq.com>
2023-02-09 23:39:24 +0800

    完善环境的搭建

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-
+import babel from "rollup-plugin-babel"
+import serve from "rollup-plugin-serve"
+import livereload from 'rollup-plugin-livereload'
 export default {
     input:'package/index.js',
     output:{
-        format:'esm',
-        name:'3DEarth',
+        format:'iife',
+        name:'Threejs3DEarth',
         dir:'dist'
     },
     plugins:[
         commonjs(),
-        resolve()
+        resolve(),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        livereload(),
+        serve({
+            open: true,
+            openPage:'/examples/index.html',
+            port: 8000
+        })
     ]
 }
