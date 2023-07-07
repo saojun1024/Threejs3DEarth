f3bbdee280ddb84b8179ba9a330424294c2b4f25
saojun1024 <1341313944@qq.com>
2023-07-07 08:41:30 +0800

    aaa

--- examples/earth2/main.js
+++ examples/earth2/main.js
@@ -1,64 +1,38 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// 工具类函数
+var geometry = new THREE.BufferGeometry();
+var positions = [];
+var count = 3000; // 点的数量
 
+// 生成随机的点位置
+for (var i = 0; i < count; i++) {
+  var phi = Math.acos(-1 + (2 * i) / count);
+  var theta = Math.sqrt(count * Math.PI) * phi;
 
-function longlatiToXYZ(){
-    const spherical = new THREE.Spherical
-    spherical.radius = 100 // 半径
-    
+  var x = Math.cos(theta) * Math.sin(phi);
+  var y = Math.sin(theta) * Math.sin(phi);
+  var z = Math.cos(phi);
 
+  positions.push(x, y, z);
 }
 
+geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
 
+var material = new THREE.PointsMaterial({ color: 0x33a1c9, size: 0.01 });
 
+var points = new THREE.Points(geometry, material);
+scene.add(points);
 
+camera.position.z = 3;
 
-
-// 创建场景
-const scene = new THREE.Scene();
-
-// 创建相机
-const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000);
-camera.position.set(0,10,10)
-camera.lookAt(0,0,0)
-// 创建渲染器
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(1200,800);
-document.body.appendChild(renderer.domElement);
-const gridHelper = new THREE.GridHelper(20,20,0x409eff,0x409eff);
-scene.add(gridHelper);
-
-// 创建一个轴线
-const axesHelper = new THREE.AxesHelper(100);
-scene.add(axesHelper);
-
-
-
-
-const container = new THREE.Object3D()
-scene.add(container);
-
-
-
-
-
-const geometry = new THREE.BoxGeometry(6, 6, 6);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube1 = new THREE.Mesh(geometry, material);
-
-
-const geometry2 = new THREE.BoxGeometry(4, 4, 4);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xCC99FF});
-const cube2 = new THREE.Mesh(geometry2, material2);
-scene.add(cube1)
-scene.add(cube2)
-
-
-
-
-// 渲染循环
 function animate() {
-  //requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
+  points.rotation.x += 0.001;
+  points.rotation.y += 0.001;
   renderer.render(scene, camera);
 }
 
